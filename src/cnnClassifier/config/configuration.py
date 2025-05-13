1d5424c13a38278d7dc2dbb26454743266b17605
franztac <fratacc91@gmail.com>
2025-05-13 13:29:58 +0200

    prepare callbacks added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -5,6 +5,7 @@ from src.cnnClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from src.cnnClassifier.entity.config_entity import (
     DataIngestionConfig,
     PrepareBaseModelConfig,
+    PrepareCallbackConfig,
 )
 
 
@@ -48,3 +49,22 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+    def get_prepare_callback_config(self) -> PrepareCallbackConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_callback_dir = os.path.dirname(config.checkpoint_model_filepath)
+
+        create_directories(
+            [
+                Path(model_ckpt_callback_dir),
+                Path(config.tensorboard_root_log_dir),
+            ]
+        )
+
+        prepare_callback_config = PrepareCallbackConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
+        )
+
+        return prepare_callback_config
